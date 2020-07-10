b946007a5dc20554273b0ec9078494ea36eb9844
exts <lamonteh2@gmail.com>
2020-07-10 11:24:57 -0500

    oops forgot to change app name

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Week In Review',
+      title: 'Label Manager',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
